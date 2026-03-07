b3be731a1d0e368657b55039263cd1436fa1da51
MonacoBz <gerardossj2@hotmail.com>
2026-03-07 13:13:00 -0600

    Commit in Blank

---
+++ src/main/java/com/app/monacohub/MonacoHubApplication.java
@@ -0,0 +1,13 @@
+package com.app.monacohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonacoHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonacoHubApplication.class, args);
+    }
+
+}
